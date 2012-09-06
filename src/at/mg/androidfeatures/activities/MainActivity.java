485bbb74c1588f1b7f215a3bfe33a255a42a9855
Markus <dreidmg@gmail.com>
2012-09-06 21:37:40 +0200

    add strings

--- src/at/mg/androidfeatures/activities/MainActivity.java
+++ src/at/mg/androidfeatures/activities/MainActivity.java
@@ -2,6 +2,7 @@ package at.mg.androidfeatures.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Html;
 import at.mg.androidfeatures.R;
 import at.mg.androidfeatures.R.layout;
 import at.mg.androidfeatures.activities.parents.AF_Activity;
@@ -13,7 +14,29 @@ public class MainActivity extends AF_Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        
+        
         setContentView(R.layout.activity_main);
+    }
+    
+    private void formatString(){
+    	//this string can not be formated
+    	String rString = getString(R.string.stop_message);
+    	Log.i(rString);
+    	
+    	//a formatable string
+    	String fString = getString(R.string.stop_message_format);
+    	String formatString = String.format(fString, " text too much");
+    	CharSequence styledString = Html.fromHtml(formatString);
+    	Log.i(styledString);
+    	
+    	//plurals
+    	String unicorns = getResources().getQuantityString(R.plurals.unicornCount, 1,1);
+    	Log.i(unicorns);
+    	unicorns = getResources().getQuantityString(R.plurals.unicornCount, 2,2);
+    	Log.i(unicorns);
+    	unicorns = getResources().getQuantityString(R.plurals.unicornCount, 0, "f", "g");
+    	Log.i(unicorns);
     }
     
 
